dd765181eb343810293c195ada3c33283c637504
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-05 09:08:53 -0500

    Migrate unit tests

---
+++ tests/testthat/test-selectSamples.R
@@ -0,0 +1,13 @@
+context("selectSamples")
+
+test_that("SingleCellExperiment", {
+    object <- selectSamples(sce, sampleId = "sample1")
+    expect_identical(
+        object = sampleNames(object),
+        expected = c(sample1 = "sample1")
+    )
+    expect_identical(
+        object = rownames(sampleData(object)),
+        expected = "sample1"
+    )
+})
