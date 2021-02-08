2bc5b04cccd43e229577e8169bb190f7dcdcd7c8
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-08 15:32:57 -0500

    Update NAMESPACE

--- R/reexports.R
+++ R/reexports.R
@@ -9,3 +9,29 @@ AcidExperiment::sampleNames
 #' @importFrom AcidExperiment sampleNames<-
 #' @export
 AcidExperiment::`sampleNames<-`
+
+
+
+#' @importFrom SingleCellExperiment reducedDim
+#' @export
+SingleCellExperiment::reducedDim
+
+#' @importFrom SingleCellExperiment reducedDim<-
+#' @export
+SingleCellExperiment::`reducedDim<-`
+
+#' @importFrom SingleCellExperiment reducedDimNames
+#' @export
+SingleCellExperiment::reducedDimNames
+
+#' @importFrom SingleCellExperiment reducedDimNames<-
+#' @export
+SingleCellExperiment::`reducedDimNames<-`
+
+#' @importFrom SingleCellExperiment reducedDims
+#' @export
+SingleCellExperiment::reducedDims
+
+#' @importFrom SingleCellExperiment reducedDims<-
+#' @export
+SingleCellExperiment::`reducedDims<-`
