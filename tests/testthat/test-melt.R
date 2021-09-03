47ee6bcd40c3d0a3f01d7c8c350957dad774ec49
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-03 10:04:08 -0400

    Improve code coverage

---
+++ tests/testthat/test-melt.R
@@ -0,0 +1,17 @@
+context("melt")
+
+test_that("SCE", {
+    x <- melt(sce)
+    expect_s4_class(x, "DataFrame")
+    expect_identical(
+        object = colnames(x),
+        expected = c(
+            "rowname",
+            "colname",
+            "value",
+            "sampleId",
+            "sampleName",
+            "interestingGroups"
+        )
+    )
+})
