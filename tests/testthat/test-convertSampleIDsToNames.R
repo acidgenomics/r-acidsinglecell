da3ef3775ff2766e1b201298fc0350fcb7f253cc
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-03 09:51:24 -0400

    Improve code coverage

---
+++ tests/testthat/test-convertSampleIDsToNames.R
@@ -0,0 +1,12 @@
+context("convertSampleIDsToNames")
+
+test_that("SCE", {
+    expect_message(
+        object = convertSampleIDsToNames(sce),
+        regexp = "unmodified"
+    )
+    expect_identical(
+        object = convertSampleIDsToNames(sce),
+        expected = sce
+    )
+})
