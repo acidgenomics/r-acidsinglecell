4f2c8cd64630d1b3e5561b8d071df4ce0832ecca
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-14 09:22:33 -0400

    Rework code that offloads to scater

--- R/cpm-methods.R
+++ R/cpm-methods.R
@@ -1,12 +1,7 @@
-## FIXME Consider moving this to AcidSingleCell.
-## FIXME If that is the case, scater needs to be added as a suggested package.
-
-
-
 #' @name cpm
 #' @inherit AcidGenerics::cpm
 #' @keywords internal
-#' @note Updated 2020-01-30.
+#' @note Updated 2021-09-14.
 #'
 #' @inheritParams AcidRoxygen::params
 #'
@@ -16,11 +11,7 @@
 #' - `scater::calculateCPM()`.
 #'
 #' @examples
-#' data(
-#'     Seurat,
-#'     SingleCellExperiment,
-#'     package = "AcidTest"
-#' )
+#' data(SingleCellExperiment, package = "AcidTest")
 #'
 #' ## SingleCellExperiment ====
 #' object <- SingleCellExperiment
@@ -28,27 +19,27 @@
 #' cpm <- cpm(object)
 #' class(cpm)
 #' mean(cpm)
-#'
-#' ## Seurat ====
-#' object <- Seurat
-#' cpm <- cpm(object)
-#' class(cpm)
-#' mean(cpm)
 NULL
 
 
 
-## Updated 2020-01-30.
+## Updated 2021-09-14.
 `cpm,SCE` <-  # nolint
     function(object) {
         ## Early return if cpm assay is defined.
         if (isSubset("cpm", assayNames(object))) {
             return(assay(x = object, i = "cpm"))
         }
-        ## Otherwise, calculate on the fly.
-        assert(is.numeric(sizeFactors(object)))
-        alert("Calculating CPM with {.pkg scater}::{.fun calculateCPM}.")
-        calculateCPM(object)
+        assert(
+            is.numeric(sizeFactors(object)),
+            msg = "Size factors are not defined in object."
+        )
+        alert(sprintf(
+            "Calculating CPM with {.pkg %s}::{.fun %s}.",
+            "scater", "calculateCPM"
+        ))
+        requireNamespaces("scater")
+        scater::calculateCPM(object)
     }
 
 
