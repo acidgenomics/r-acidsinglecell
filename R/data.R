58a5ad1c2e16a035ca91d31020079666e31e1aff
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-14 09:34:17 -0400

    Save KnownMarkers example

--- R/data.R
+++ R/data.R
@@ -22,12 +22,12 @@
 
 
 
-# #' Known markers example
-# #'
-# #' @note Updated 2021-09-14.
-# #' @return `KnownMarkers`.
-# #' @usage data(km)
-# #' @examples
-# #' data(km)
-# #' print(km)
-# "km"
+#' Known markers example
+#'
+#' @note Updated 2021-09-14.
+#' @return `KnownMarkers`.
+#' @usage data(km)
+#' @examples
+#' data(km)
+#' print(km)
+"km"
