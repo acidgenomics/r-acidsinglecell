4c342b77be0af67f50892e3e4a35a17846b8d59f
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-14 09:03:18 -0400

    Migrate code from pointillism

---
+++ R/clusters-methods.R
@@ -0,0 +1,44 @@
+#' @name clusters
+#' @inherit AcidGenerics::clusters
+#' @note Updated 2021-09-13.
+#'
+#' @inheritParams AcidRoxygen::params
+#' @param ... Additional arguments.
+#'
+#' @seealso
+#' - `Seurat::Idents()`.
+#' - `monocle3::clusters()`.
+#'
+#' @examples
+#' data(SingleCellExperiment_Seurat, package = "AcidTest")
+#'
+#' ## Seurat ====
+#' object <- SingleCellExperiment_Seurat
+#' x <- clusters(object)
+#' head(x)
+#' table(x)
+NULL
+
+
+
+## Updated 2019-08-06.
+`clusters,SCE` <-  # nolint
+    function(object) {
+        validObject(object)
+        assert(isSubset("ident", colnames(colData(object))))
+        ident <- colData(object)[["ident"]]
+        assert(is.factor(ident))
+        names(ident) <- colnames(object)
+        ident
+    }
+
+
+
+#' @rdname clusters
+#' @export
+setMethod(
+    f = "clusters",
+    signature = signature("SingleCellExperiment"),
+    definition = `clusters,SCE`
+)
+
