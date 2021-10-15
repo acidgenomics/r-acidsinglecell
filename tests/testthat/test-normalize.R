5d61fc01054486fa87dcac24a910da7bbb2f8c34
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-15 10:32:19 -0400

    Improve code coverage

--- tests/testthat/test-normalize.R
+++ tests/testthat/test-normalize.R
@@ -3,6 +3,7 @@ context("normalize")
 test_that("SCE", {
     object <- sce_seurat
     object <- normalize(object)
-    expect_true(is.numeric(sizeFactors(object)))
+    expect_s4_class(object, "SingleCellExperiment")
     expect_s4_class(logcounts(object), "Matrix")
+    expect_true(is.numeric(sizeFactors(object)))
 })
