bef8650c5c52180a89707727a361de3486f6acb1
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-02 07:34:24 -0500

    Rework the NAMESPACE

---
+++ R/imports.R
@@ -0,0 +1,70 @@
+## Classes ====================================================================
+
+#' @importClassesFrom IRanges DataFrameList SplitDataFrameList
+#' @importClassesFrom Matrix Matrix
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment
+#'   SummarizedExperiment
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom AcidGenerics aggregateCellsToSamples barcodeRanksPerSample
+#'   calculateMetrics camelCase cell2sample cellCountsPerCluster
+#'   cellTypesPerCluster cpm clusters convertSampleIDsToNames encode
+#'   interestingGroups leftJoin mutateAll mutateIf snakeCase
+#'   uniteInterestingGroups
+#' @importFrom Biobase sampleNames
+#' @importFrom BiocGenerics colSums combine counts counts<- do.call
+#'   estimateSizeFactors rowSums normalize sizeFactors
+#' @importFrom BiocIO export import
+#' @importFrom S4Vectors DataFrame SimpleList aggregate complete.cases decode
+#'   head metadata metadata<- na.omit split tail
+#' @importFrom SingleCellExperiment SingleCellExperiment reducedDimNames
+#'   reducedDim
+#' @importFrom SummarizedExperiment assay assayNames assays colData colData<-
+#'   rowRanges
+#' @importFrom methods coerce show
+#'
+#' @importMethodsFrom AcidExperiment aggregate calculateMetrics combine decode
+#'   encode estimateSizeFactors export interestingGroups matchSampleColumn
+#'   sampleNames uniteInterestingGroups
+#' @importMethodsFrom AcidPlyr leftJoin mutateAll mutateIf
+#' @importMethodsFrom SingleCellExperiment coerce
+#' @importMethodsFrom pipette coerce export import
+#' @importMethodsFrom syntactic camelCase snakeCase
+NULL
+
+
+
+## S3 generics and methods =====================================================
+
+#' @importFrom pipette as_tibble
+#' @importFrom stats model.matrix relevel
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase barcodePattern basenameSansExt initDir methodFormals
+#'   methodFunction printString realpath requireNamespaces standardizeCall
+#' @importFrom AcidCLI abort alert alertInfo alertSuccess alertWarning dl h1 h2
+#'   toInlineString txt ul verbatim
+#' @importFrom AcidExperiment makeSummarizedExperiment matchInterestingGroups
+#' @importFrom AcidGenomes makeGene2SymbolFromEnsembl
+#' @importFrom IRanges DataFrameList SplitDataFrameList
+#' @importFrom Matrix Matrix
+#' @importFrom goalie allAreMatchingRegex allArePositive areDisjointSets
+#'   areSetEqual assert bapply hasColnames hasLength hasMetrics hasNames
+#'   hasNoDuplicates hasNonzeroRowsAndCols hasRownames hasRows hasValidNames
+#'   isAny isCharacter isFlag isInLeftOpenRange isInRange isInstalled isInt
+#'   isIntegerish isNonNegative isPositive isScalar isString isSubset validate
+#' @importFrom methods as is new setClass setValidity validObject
+#' @importFrom pipette assignAndSaveData tibble
+#' @importFrom scales percent
+#' @importFrom stringr str_match
+#' @importFrom utils packageName packageVersion
+NULL
--- R/package.R
+++ R/package.R
@@ -1,40 +1,8 @@
+## Package description =========================================================
 #' AcidSingleCell
 #'
 #' Toolkit for single-cell RNA-seq analysis that extends the functionality
 #' of SingleCellExperiment.
 #'
 #' @keywords internal
-#'
-#' @importClassesFrom SingleCellExperiment SingleCellExperiment
-#' @importClassesFrom pipette Matrix
-#'
-#' @importMethodsFrom SingleCellExperiment coerce
-#' @importMethodsFrom pipette coerce
-#'
-#' @importFrom AcidBase barcodePattern initDir methodFormals methodFunction
-#'   model.matrix packageName packageVersion printString realpath relevel
-#'   requireNamespaces standardizeCall
-#' @importFrom AcidCLI abort alert alertInfo alertSuccess alertWarning dl h1 h2
-#'   toInlineString txt ul verbatim
-#' @importFrom AcidExperiment assay assayNames assays calculateMetrics colData
-#'   colData<- counts counts<- decode encode estimateSizeFactors
-#'   interestingGroups makeSummarizedExperiment matchInterestingGroups
-#'   matchSampleColumn rowRanges sampleNames sizeFactors uniteInterestingGroups
-#' @importFrom AcidGenerics DataFrame DataFrameList SimpleList
-#'   SplitDataFrameList coerce colSums complete.cases do.call head metadata
-#'   metadata<- na.omit rowSums split tail
-#' @importFrom AcidGenomes makeGene2SymbolFromEnsembl
-#' @importFrom AcidPlyr leftJoin mutateAll mutateIf
-#' @importFrom SingleCellExperiment SingleCellExperiment reducedDimNames
-#'   reducedDim
-#' @importFrom goalie allAreMatchingRegex allArePositive areDisjointSets
-#'   areSetEqual assert bapply hasColnames hasLength hasMetrics hasNames
-#'   hasNoDuplicates hasNonzeroRowsAndCols hasRownames hasRows hasValidNames
-#'   isAny isCharacter isFlag isInLeftOpenRange isInRange isInt isIntegerish
-#'   isNonNegative isPositive isScalar isString isSubset validate
-#' @importFrom methods as is new setClass setValidity validObject
-#' @importFrom pipette Matrix as_tibble assignAndSaveData import tibble
-#' @importFrom scales percent
-#' @importFrom stringr str_match
-#' @importFrom syntactic camelCase snakeCase
 "_PACKAGE"
