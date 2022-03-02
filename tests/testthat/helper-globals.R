3284a48938ac97f2980441035bd88038ae406deb
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-02 07:47:48 -0500

    Update working examples

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -5,25 +5,26 @@ data(
 )
 data(
     RangedSummarizedExperiment,
-    SingleCellExperiment,
     SingleCellExperiment_Seurat,
     SingleCellExperiment_lanesplit,
+    SingleCellExperiment_splatter,
     package = "AcidTest",
     envir = environment()
 )
 
 ## nolint start
 rse <- RangedSummarizedExperiment
-sce <- SingleCellExperiment
+sce <- SingleCellExperiment_splatter
 sce_seurat <- SingleCellExperiment_Seurat
 sce_lanesplit <- SingleCellExperiment_lanesplit
 ## nolint end
 
 ## nolint start
-`rowData<-` <- AcidExperiment::`rowData<-`
+`rowData<-` <- SummarizedExperiment::`rowData<-`
 SingleCellExperiment <- SingleCellExperiment::SingleCellExperiment
 aggregateCols <- AcidExperiment::aggregateCols
 aggregateRows <- AcidExperiment::aggregateRows
-rowData <- AcidExperiment::rowData
+lowcounts <- SingleCellExperiment::logcounts
+rowData <- SummarizedExperiment::rowData
 simpleClass <- AcidBase::simpleClass
 ## nolint end
