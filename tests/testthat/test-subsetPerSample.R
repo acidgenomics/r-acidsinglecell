669088835c5dbf1116ba817e24ed3e24d75d0046
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-10 17:08:48 -0500

    Improve unit tests

--- tests/testthat/test-subsetPerSample.R
+++ tests/testthat/test-subsetPerSample.R
@@ -1,45 +1,56 @@
 context("subsetPerSample")
 
 test_that("List mode", {
+    object <- sce
+    n <- c(
+        "sample1" = 95L,
+        "sample2" = 96L,
+        "sample3" = 121L,
+        "sample4" = 88L
+    )
+    expect_identical(
+        object = summary(colData(object)[["sampleId"]]),
+        expected = n
+    )
     x <- subsetPerSample(sce, assignAndSave = FALSE)
     expect_is(x, "list")
     expect_identical(
         object = names(x),
-        expected = paste0("sample", seq_len(2L))
+        expected = paste0("sample", seq_len(4L))
     )
     expect_identical(
-        lapply(x, simpleClass),
-        list(
-            sample1 = "SingleCellExperiment",
-            sample2 = "SingleCellExperiment"
-        )
+        object = vapply(X = x, FUN = ncol, FUN.VALUE = integer(1L)),
+        expected = n
     )
 })
 
 ## This is useful for larger datasets.
 test_that("Assign and save mode", {
-    env <- new.env()
+    object <- sce
+    envir <- new.env()
     files <-
         subsetPerSample(
-            object = sce,
+            object = object,
             assignAndSave = TRUE,
-            envir = env,
+            envir = envir,
             dir = "subsetPerSample"
         )
     expect_identical(
         object = files,
         expected = c(
-            sample1 = realpath(file.path("subsetPerSample", "sample1.rds")),
-            sample2 = realpath(file.path("subsetPerSample", "sample2.rds"))
+            "sample1" = realpath(file.path("subsetPerSample", "sample1.rds")),
+            "sample2" = realpath(file.path("subsetPerSample", "sample2.rds")),
+            "sample3" = realpath(file.path("subsetPerSample", "sample3.rds")),
+            "sample4" = realpath(file.path("subsetPerSample", "sample4.rds"))
         )
     )
     expect_identical(
-        object = ls(env),
-        expected = paste0("sample", seq_len(2L))
+        object = ls(envir),
+        expected = paste0("sample", seq_len(4L))
     )
     expect_identical(
         object = sort(list.files("subsetPerSample")),
-        expected = paste0("sample", seq_len(2L), ".rds")
+        expected = paste0("sample", seq_len(4L), ".rds")
     )
     unlink("subsetPerSample", recursive = TRUE)
 })
