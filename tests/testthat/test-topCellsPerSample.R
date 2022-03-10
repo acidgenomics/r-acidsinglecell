0c1b2a5b54f256c205c88b11dbab53c2e17d2fb6
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-10 17:10:17 -0500

    Update code coverage

--- tests/testthat/test-topCellsPerSample.R
+++ tests/testthat/test-topCellsPerSample.R
@@ -1,10 +1,11 @@
 context("topCellsPerSample")
 
 test_that("SingleCellExperiment", {
-    x <- topCellsPerSample(sce, n = 5L)
+    object <- sce
+    x <- topCellsPerSample(object = object, n = 5L)
     expect_is(x, "list")
     expect_identical(
         object = names(x),
-        expected = paste0("sample", seq(2L))
+        expected = paste0("sample", seq(4L))
     )
 })
