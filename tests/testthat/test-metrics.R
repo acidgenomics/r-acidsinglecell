d640deafd40c0a44f01187728d4f2df82bc550af
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-04 16:48:44 -0400

    Work on updating unit tests

--- tests/testthat/test-metrics.R
+++ tests/testthat/test-metrics.R
@@ -1,25 +1,7 @@
 context("metrics")
 
-test_that("SCE : tbl_df", {
-    object <- metrics(sce, return = "tbl_df")
-    expect_s3_class(object, "tbl_df")
-    expect_identical(
-        object = colnames(object),
-        expected = c(
-            "cellId",
-            "sampleId",
-            "sampleName",
-            "interestingGroups"
-        )
-    )
-    expect_identical(
-        object = object[["cellId"]][[1L]],
-        expected = "cell001"
-    )
-})
-
-test_that("SCE : DataFrame", {
-    object <- metrics(sce, return = "DataFrame")
+test_that("SCE", {
+    object <- metrics(sce)
     expect_s4_class(object, "DataFrame")
     expect_identical(
         object = colnames(object),
